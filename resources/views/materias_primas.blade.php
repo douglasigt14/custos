4714794a157f58e1f6978ae65d78bbffd7f1f560
Douglas Rocha <douglas_igt14@hotmail.com>
2021-03-09 09:43:37 -0300

    Unidade de Medida

--- resources/views/materias_primas.blade.php
+++ resources/views/materias_primas.blade.php
@@ -13,9 +13,11 @@
 								<table class="table table-striped myTable">
 									<thead>
 										<tr>
-											<th>Cod'</th>
+											<th>Cod</th>
 											<th>Item</th>
 											<th class='center'>Data Atualiza</th>
+											<th class='center'>Unid. Med.</th>
+											
 											<th class='center'>Custo Focco</th>
 											<th class='center'>Custo Futuro</th>
 											<th class='center'>Perc. (%)</th>
@@ -26,9 +28,11 @@
 										<tr>
 											<td>{{$item->cod_item}}</td>
 											<td>{{$item->desc_tecnica}}</td>
+											
 											<td class='center'>{{date("d/m/Y", strtotime($item->dt_atualiza))}}</td>
-											<td class='center'>{{number_format($item->valor,4,',','.')}}</td>
-											<td class='center'>{{number_format($item->valor,4,',','.')}}</td>
+											<td class='center'>{{$item->unid_med}}</td>
+											<td class='center texto-verde'>{{number_format($item->valor,4,',','.')}}</td>
+											<td class='center texto-azul'>{{number_format($item->valor,4,',','.')}}</td>
 											<td class='center'>0%</td>
 										</tr>
 										@endforeach
