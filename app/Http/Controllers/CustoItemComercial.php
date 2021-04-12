d3b64535cc3415ce5cd25f164b2195bbf1e72d21
Douglas Rocha <douglas_igt14@hotmail.com>
2021-04-12 08:55:04 -0300

    Consulta Estrutura

--- app/Http/Controllers/CustoItemComercial.php
+++ app/Http/Controllers/CustoItemComercial.php
@@ -3,10 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
 
 class CustoItemComercial extends Controller
 {
     public function index(){
-        return view('custo_item_comercial', compact([]));
+        $sql = "SELECT 
+                    * 
+                FROM 
+                    FOCCO3i.LJ_EST_SISTEMA_CUSTO 
+                WHERE  
+                    codprodutopai = '6572'
+                AND idcorpai = '48377'";
+        
+        $itens = DB::connection('oracle')->select($sql);
+        dd($itens);
+        
+
+        return view('custo_item_comercial', compact(["itens"]));
     }
 }
