15c64f96a624292bc2883a0bf0d9e5ba55165b21
Douglas Rocha <douglas_igt14@hotmail.com>
2021-04-16 16:01:04 -0300

    Alterei Kernel dos Commands

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,6 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
         $schedule->command('custo:cron')->dailyAt('12:00');
-        $schedule->command('custo:apagar');
     }
 
     /**
