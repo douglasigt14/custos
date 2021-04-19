c7014181e6b5771c23c343e820433ab3355b4e0d
Douglas Rocha <douglas_igt14@hotmail.com>
2021-04-19 08:06:25 -0300

    Evolução do Cron

--- app/Console/Commands/CustoCron.php
+++ app/Console/Commands/CustoCron.php
@@ -38,9 +38,23 @@ class CustoCron extends Command
      */
     public function handle()
     {
-        DB::table('teste')->insert([
-            'desc' => 'SHOW RAPAZ'
-        ]);
+        $sql_lista_itens = "SELECT 
+        COD_ITEM
+        ,ITEM 
+    FROM 
+        FOCCO3I.LJ_VALOR_ITEM_CUSTO
+    GROUP BY 
+        COD_ITEM
+        ,ITEM";
+
+        $lista_itens  = DB::connection('oracle')->select($sql_lista_itens);
+        
+        //dd($lista_itens);
+        foreach ($lista_itens as $key => $item) {
+            dd($item);
+        }
+        //$this->info('Example Cron comando rodando com êxito');
+
         return 0;
     }
 }
