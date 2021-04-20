e8b998ab744bc5b3cea1e236ba715c298ac5a535
Douglas Rocha <douglas_igt14@hotmail.com>
2021-04-20 07:55:23 -0300

    Iniciei para puxar preço com comissão de 5

--- app/Http/Controllers/MargemLucro.php
+++ app/Http/Controllers/MargemLucro.php
@@ -13,7 +13,42 @@ class MargemLucro extends Controller
 
             $itens  = DB::connection('mysql')->select($sql);
 
-        
+        foreach ($itens as $key => $item) {
+            $sql_preco = "SELECT TITENS.COD_ITEM
+                                ,TITENS.DESC_TECNICA
+                                ,TPRECOSVEN_IT.PRECO     AS PRECO_TABELA
+                                ,TDESC_IPCMD.VLR_MAXIMO  AS VALOR_DESC
+                                ,ROUND(TPRECOSVEN_IT.PRECO - ((TPRECOSVEN_IT.PRECO * TDESC_IPCMD.VLR_MAXIMO)/100),2) AS VALOR_C_DESC
+                        FROM TPOLITICA_COM_DESC 
+                            ,TITENS_PCMD
+                            ,TDESC_IPCMD
+                            ,TITENS_COMERCIAL
+                            ,TITENS_EMPR
+                            ,TITENS
+                            ,TPRECOSVEN
+                            ,TPRECOSVEN_IT
+                        WHERE TITENS_PCMD.PCMD_ID         =  TPOLITICA_COM_DESC.ID
+                        AND   TDESC_IPCMD.IPCMD_ID        =  TITENS_PCMD.ID
+                        AND   TITENS_COMERCIAL.ID         =  TITENS_PCMD.ITCM_ID
+                        AND   TITENS_COMERCIAL.ITEMPR_ID  =  TITENS_EMPR.ID
+                        AND   TITENS.ID                   =  TITENS_EMPR.ITEM_ID
+                        AND   TITENS_PCMD.TPRVEN_ID       =  TPRECOSVEN.ID
+                        AND   TPRECOSVEN.ID               =  TPRECOSVEN_IT.TPRVEN_ID
+                        AND   TPRECOSVEN_IT.ITCM_ID       =  TITENS_COMERCIAL.ID
+                        AND   TDESC_IPCMD.DESCRICAO LIKE '%DESC COMISSÃO 5%'
+                        AND   TPRECOSVEN.COD_PREVEN = 1
+                        AND   TPRECOSVEN_IT.SIT = 1
+                        AND   TITENS.COD_ITEM = $item->cod_item
+                        --AND   RANKING = 80
+                        ORDER BY DESC_TECNICA";
+            
+            $precos = DB::connection('oracle')->select($sql_preco);
+            
+            $item->prreco_com_5 = $precos[0]->valor_c_desc ?? NULL;
+
+        }
+
+       // dd($itens);
 
         return view('margem_lucro', compact(["itens"]));
     }
