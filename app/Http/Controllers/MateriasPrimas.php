f4720e7cd93e8c3535a6ee08aa24675e66e2cb4c
Douglas Rocha <douglas_igt14@hotmail.com>
2021-04-21 09:24:36 -0300

    Filtro pela Obserção da TITENS CUSTOS

--- app/Http/Controllers/MateriasPrimas.php
+++ app/Http/Controllers/MateriasPrimas.php
@@ -37,6 +37,7 @@ class MateriasPrimas extends Controller
     AND TITENS_CUSTOS.VLR_CST_MAT_DIR <> 0
     AND TITENS.DESC_TECNICA NOT LIKE '--%'
     AND TITENS.DESC_TECNICA NOT LIKE '(P)%'
+    AND TITENS_CUSTOS.OBSER =  1
     ORDER BY TGRP_CLAS_ITE.COD_GRP_ITE";
         $itens = DB::connection('oracle')->select($sqlItens);
     
