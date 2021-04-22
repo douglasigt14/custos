bcccd16083723bfde4b19d26a55026c2655bd485
Douglas Rocha <douglas_igt14@hotmail.com>
2021-04-22 07:42:09 -0300

    Comecei tabela de avaliação

--- resources/views/avaliacao_custo.blade.php
+++ resources/views/avaliacao_custo.blade.php
@@ -9,7 +9,21 @@
 							<h3 class="panel-title">Avaliação do Vlr Compra x Vlr Reposição </h3>
 						</div>
 						<div class="panel-body">
-							
+							<div class="row"></div>
+							<div class="row">
+								<div class="col md-12">
+									<table class="table table-hover menor myTable">
+										<thead>
+											<tr>
+												
+											</tr>
+										</thead>
+										<tbody>
+
+										</tbody>
+									</table>
+								</div>
+							</div>
 							
                         </div>
                     </div>
