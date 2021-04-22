9ac454d573b38808a3bdbf97ba9233e7dc23c347
Douglas Rocha <douglas_igt14@hotmail.com>
2021-04-22 07:45:42 -0300

    Iniciei form Buscar

--- resources/views/avaliacao_custo.blade.php
+++ resources/views/avaliacao_custo.blade.php
@@ -9,7 +9,18 @@
 							<h3 class="panel-title">Avaliação do Vlr Compra x Vlr Reposição </h3>
 						</div>
 						<div class="panel-body">
-							<div class="row"></div>
+							<div class="row">
+								<div class="col col-md-3">
+									<input type="date" name="dt_inicial" class="form-control">
+								</div>
+								<div class="col col-md-3">
+									<input type="date" name="dt_final"  class="form-control">
+								</div>
+								<div class="col col-md-2">
+									<button type='submit' class="btn btn-primary">Buscar</button>
+								</div>
+								<div class="col col-md-4"></div>
+							</div>
 							<div class="row">
 								<div class="col md-12">
 									<table class="table table-hover menor myTable">
