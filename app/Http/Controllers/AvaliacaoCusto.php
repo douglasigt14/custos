1cf8cd9ecb96783e81b4518f9d86a8e22bf558ba
Douglas Rocha <douglas_igt14@hotmail.com>
2021-04-22 07:37:26 -0300

    Consulta avaliação de Custos

--- app/Http/Controllers/AvaliacaoCusto.php
+++ app/Http/Controllers/AvaliacaoCusto.php
@@ -3,12 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use DB;
 class AvaliacaoCusto extends Controller
 {
     public function index(){
-       
+    $dt_inicial = $_GET['dt_inicial'] ?? NULL;
+    $dt_final = $_GET['dt_inicial'] ?? NULL;
+    
+    $dados = array();
 
-        return view('avaliacao_custo', compact([]));
+    if($dt_inicial and $dt_inicial){
+        $sql = "SELECT 
+            * 
+        FROM 
+            FOCCO3I.AVALIACAO_CUSTO_SISTEMA 
+        WHERE 
+            DT_ENT_DATA BETWEEN TO_DATE ('$dt_inicial','DD/MM/RRRR') 
+        AND TO_DATE ('$dt_final','DD/MM/RRRR')";
+            
+        $dados = DB::connection('oracle')->select($sql);
+        dd($dados);
+    }
+
+        return view('avaliacao_custo', compact(["dados","dt_inicial","dt_final"]));
     }
 }
