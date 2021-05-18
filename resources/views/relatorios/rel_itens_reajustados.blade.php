dc64f15adbd0f6230f4f0e40980bd4012032535f
Douglas Rocha <douglas_igt14@hotmail.com>
2021-05-18 07:39:58 -0300

    Estilos do Cabeçario Iniciado

--- resources/views/relatorios/rel_itens_reajustados.blade.php
+++ resources/views/relatorios/rel_itens_reajustados.blade.php
@@ -1,8 +1,20 @@
-<div>
-    <img style="display: inline" src="{{asset('img/logo.png')}}" height="100" width="200">
-    <div style="text-align: left">
-    <p style="margin: -95px 0 0 230px;font-size: 19px;font-weight: bold;">TUBOARTE INDÚSTRIA E COMERCIO EIRELI</p>
-    <p style="margin: 0px 0 0 230px; font-size: 14px">RUA 12 DE AGOSTO, S/Nº - BAIRRO NOVA BRASÍLIA</p>
-    <p style="margin: 0px 0 0 230px; font-size: 14px">FONE: (88) 3522-8300 FAX: (88) 3522-8304</p>
-    <p style="margin: 0px 0 0 230px; font-size: 14px">SITE: www.tuboarte.com.br</p></div><br>
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Relatorio</title>
+    <link rel="stylesheet" href="{{asset('assets/css/main.css')}}?{{date("YmdHis")}}">
+</head>
+<body>
+    <div>
+        <img class="img-logo-cabecario" src="{{asset('img/logo.png')}}" height="100" width="200">
+        <div class="div-container-cabecario">
+        <p class='titulo-cabecario'>TUBOARTE INDÚSTRIA E COMERCIO EIRELI</p>
+        <p style="margin: 0px 0 0 230px; font-size: 14px">RUA 12 DE AGOSTO, S/Nº - BAIRRO NOVA BRASÍLIA</p>
+        <p style="margin: 0px 0 0 230px; font-size: 14px">FONE: (88) 3522-8300 FAX: (88) 3522-8304</p>
+        <p style="margin: 0px 0 0 230px; font-size: 14px">SITE: www.tuboarte.com.br</p></div><br>
+    </div>
+</body>
+</html>
