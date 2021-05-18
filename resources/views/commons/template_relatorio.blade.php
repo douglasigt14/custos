6c39b414e91177a708155876a84dc508cac9cd62
Douglas Rocha <douglas_igt14@hotmail.com>
2021-05-18 07:51:47 -0300

    Relatorio com Template

---
+++ resources/views/commons/template_relatorio.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Relatorio</title>
+    <link rel="stylesheet" href="{{asset('assets/css/main.css')}}?{{date("YmdHis")}}">
+    <script type="text/javascript">
+        function printpage()
+          {
+          window.print()
+          }
+        </script>
+    
+    @stack('styles')
+</head>
+<body onload="printpage()" class="preto-total">
+    <div>
+        <img class="img-logo-cabecario" src="{{asset('img/logo.png')}}" height="100" width="200">
+        <div class="div-container-cabecario">
+        <p class='titulo-cabecario'>TUBOARTE INDÚSTRIA E COMERCIO EIRELI</p>
+        <p class='subtitulo-cabecario'>RUA 12 DE AGOSTO, S/Nº - BAIRRO NOVA BRASÍLIA</p>
+        <p class='subtitulo-cabecario'>FONE: (88) 3522-8300 FAX: (88) 3522-8304</p>
+        <p class='subtitulo-cabecario'>SITE: www.tuboarte.com.br</p></div><br>
+
+        <center><h2>@yield('titulo')</h2></center>
+
+        @yield('conteudo')
+    </div>
+</body>
+</html>
--- resources/views/relatorios/rel_itens_reajustados.blade.php
+++ resources/views/relatorios/rel_itens_reajustados.blade.php
@@ -1,28 +1,3 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Relatorio</title>
-    <link rel="stylesheet" href="{{asset('assets/css/main.css')}}?{{date("YmdHis")}}">
-    <script type="text/javascript">
-        function printpage()
-          {
-          window.print()
-          }
-        </script>
-</head>
-<body onload="printpage()" class="preto-total">
-    <div>
-        <img class="img-logo-cabecario" src="{{asset('img/logo.png')}}" height="100" width="200">
-        <div class="div-container-cabecario">
-        <p class='titulo-cabecario'>TUBOARTE INDÚSTRIA E COMERCIO EIRELI</p>
-        <p class='subtitulo-cabecario'>RUA 12 DE AGOSTO, S/Nº - BAIRRO NOVA BRASÍLIA</p>
-        <p class='subtitulo-cabecario'>FONE: (88) 3522-8300 FAX: (88) 3522-8304</p>
-        <p class='subtitulo-cabecario'>SITE: www.tuboarte.com.br</p></div><br>
+@extends('commons.template_relatorio')
 
-        <center><h2>ITENS REAJUSTADOS</h2></center>
-    </div>
-</body>
-</html>
+@section('titulo') ITENS REAJUSTADOS @endsection
