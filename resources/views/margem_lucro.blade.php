c3a144b000babc2207c59e90e9cd515dd6350dc1
Douglas Rocha <douglas_igt14@hotmail.com>
2021-07-05 07:36:27 -0300

    Correçao da Tabela Margem de Lucro

--- resources/views/margem_lucro.blade.php
+++ resources/views/margem_lucro.blade.php
@@ -19,6 +19,7 @@
 										<th colspan="3" class='center'>COMISSÃO DE 5%</th>
 										<th colspan="3" class='center'>COMISSÃO DE 4%</th>
 										<th colspan="3" class='center'>COMISSÃO DE 3%</th>
+										<th></th>
 									</tr>
 									<tr>
 										<th>COD</th>
