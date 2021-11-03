010bd714ec9046c469dfa8be3cd91fb5e76145e5
Douglas Rocha <douglas_igt14@hotmail.com>
2021-11-03 16:55:31 -0300

    DataList Clientes

--- resources/views/simulacao_cotacao.blade.php
+++ resources/views/simulacao_cotacao.blade.php
@@ -9,7 +9,23 @@
 			<h3 class="panel-title">Simulação de Cotação</h3>
 		</div>
 		<div class="panel-body">
-			
+			<div class="row">
+				<div class="col col-md-1">
+				</div>
+				<div class="col col-md-8">
+					<input list="clientes" class='form-control' name="cliente">
+						<datalist id="clientes">
+							@foreach ($clientes as $item)
+							   <option value="{{$item->cod_e_descricao}}">
+							@endforeach
+						</datalist>
+				</div>
+				<div class="col col-md-2">
+					<button class="btn btn-primary btn-block">Buscar</button>
+				</div>
+				<div class="col col-md-1">
+				</div>
+			</div>
 		</div>
 	</div>
 @endsection
