d49fbdb5f098310ce8be269bf8483280c1b5f9de
Douglas Rocha <douglas_igt14@hotmail.com>
2021-11-04 17:21:16 -0300

    Wip

--- resources/views/simulacao_cotacao.blade.php
+++ resources/views/simulacao_cotacao.blade.php
@@ -24,7 +24,9 @@
 				</div>
 			</div>
 			<br><br>
-			<div class="row" id='info_cliente' style='display: none'>
+			<div  id='info_cliente' style='display: none'>
+
+			<div class="row">
 				<div class="col col-md-4">
 					<label>Cliente</label>
 					<input type="text" name='cliente' id='cliente' class='form-control' readonly='true'>
@@ -33,14 +35,29 @@
 					<label>Representante</label>
 					<input type="text" name='representante' id='representante' class='form-control' readonly='true'>
 				</div>
-				<div class="col col-md-2">
+				<div class="col col-md-3">
 					<label>Aumento</label>
 					<input type="text" name='aumento' id='aumento' class='form-control'>
 				</div>
-				<div class="col col-md-2">
+				<div class="col col-md-1">
 					<label>&nbsp;</label>
-					<button class="btn btn-primary btn-block">Aplicar</button>
+					<button class="btn btn-primary">Aplicar</button>
 				</div>
+			</div><br>
+			<div class="row">
+				<div class="col col-md-4">
+					<label>Data Inicial</label>
+					<input type="date" name="dt_inicial" id="dt_inicial" class="form-control">
+				</div>
+				<div class="col col-md-4">
+					<label>Data Final</label>
+					<input type="date" name="dt_final" id="dt_final" class="form-control">
+				</div>
+				<div class="col col-md-4">
+					<label>Aliquota</label>
+					<input type="text" name="aliquota" id="aliquota" class="form-control">
+				</div>
+			</div>
 			</div>
 		</div>
 	</div>
